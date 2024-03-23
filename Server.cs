196e912098fbfa5540c92d0dd59d1ea7a7da5d2a
GetChara() <133586791+SwitchGetch@users.noreply.github.com>
2024-03-23 19:17:42 +0300

    Update Server.cs

--- Server.cs
+++ Server.cs
@@ -2,64 +2,7 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 
-
-enum ErrorMessage
-{
-    None,
-    UserWithTheSameLoginIsAlreadyExist,
-    WrongLoginOrPassword
-}
-
-class User
-{
-    public string Login { get; set; }
-
-    public string Password { get; set; }
-
-    public string ID { get; set; }
-
-    public bool IsNewUser { get; set; }
-}
-
-class Message
-{
-    public bool IsAllOK { get; set; }
-
-    public ErrorMessage error { get; set; }
-}
-
-
-class Interaction
-{
-    public static async Task WriteToStream(TcpClient client, string message)
-    {
-        var stream = client.GetStream();
-        message += '\0';
-
-        byte[] bytes = Encoding.UTF8.GetBytes(message.ToArray());
-
-        await stream.WriteAsync(bytes);
-    }
-
-
-    public static string ReadfromStream(TcpClient client)
-    {
-        var stream = client.GetStream();
-        List<byte> bytes = new List<byte>();
-
-
-        int bytes_read = 0;
-
-        while ((bytes_read = stream.ReadByte()) != '\0')
-        {
-            bytes.Add((byte)bytes_read);
-        }
-
-        string str = Encoding.UTF8.GetString(bytes.ToArray());
-
-        return str;
-    }
-}
+using Classes;
 
 
 class Server
@@ -67,46 +10,44 @@ class Server
     public static List<TcpClient> Clients = new List<TcpClient>();
     public static List<User> Users = new List<User>();
 
-    public static bool CheckUser(User user)
+    public static void CheckUser(User user, ref Message answer)
     {
-        if (
-            user.IsNewUser && Users.Find(x => x.Login == user.Login) == null ||
-            !user.IsNewUser && Users.Find(x => x.Login == user.Login && x.Password == user.Password) != null
-            )
+        if (user.IsNewUser)
         {
-            return true;
+            if (Users.Find(x => x.Login == user.Login) != null)
+            {
+                answer.IsAllOK = false;
+                answer.error = ErrorMessage.UserWithTheSameLoginIsAlreadyExist;
+            }
+            else
+            {
+                user.IsNewUser = false;
+                Users.Add(user);
+            }
         }
-
-        return false;
+        else
+        {
+            if (Users.Find(x => x.Login == user.Login && x.Password == user.Password) == null)
+            {
+                answer.IsAllOK = false;
+                answer.error = ErrorMessage.WrongLoginOrPassword;
+            }
+        }
     }
 
     public static async Task ProcessClient(TcpClient client)
     {
         while (true)
         {
-            User user = Newtonsoft.Json.JsonConvert.DeserializeObject<User>(Interaction.ReadfromStream(client));
-            
+            string user_message = Interaction.ReadfromStream(client);
 
+            User user = Newtonsoft.Json.JsonConvert.DeserializeObject<User>(user_message);
 
-            Message answer = new Message();
+            Message answer = new Message() { IsAllOK = true, error = ErrorMessage.None };
 
-            answer.IsAllOK = CheckUser(user);
-            
-            if (answer.IsAllOK)
-            {
-                Users.Add(user);
-                answer.error = ErrorMessage.None;
-            }
-            else
-            {
-                answer.error = (user.IsNewUser ? ErrorMessage.UserWithTheSameLoginIsAlreadyExist : ErrorMessage.WrongLoginOrPassword);
-            }
+            CheckUser(user, ref answer);
 
-
-            string str_answer = Newtonsoft.Json.JsonConvert.SerializeObject(answer);
-
-            
-            await Interaction.WriteToStream(client, str_answer);
+            await Interaction.WriteToStream(client, Newtonsoft.Json.JsonConvert.SerializeObject(answer));
         }
     }
 
